31e7a8ab24b338a2f4d9ffcc0310ef8d0edba879
mob <zx14993@163.com>
2018-10-30 10:37:36 +0800

    Initial Commit

---
+++ DrawAndRendering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawAndRendering
+//
+//  Created by mob on 2018/10/30.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawAndRendering/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawAndRendering
+//
+//  Created by mob on 2018/10/30.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
